82837b481b662f6c8ef6bd584c51b89647d2e59d
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-04-09 19:46:38 -0400

    Add more email parsing formats

--- generate_email_address.py
+++ generate_email_address.py
@@ -10,10 +10,12 @@ def generate_email_address(first_name, last_name, email, company_name):
         logger.info("Using provided email address.")
         return email
     elif first_name and last_name:
-        email1 = f"{first_name.lower()}.{last_name.lower()}@{company_name.lower()}.com"
-        email2 = f"{first_name.lower()}{last_name.lower()}@{company_name.lower()}.com"
-        logger.info(f"Generated email addresses: '{email1}', '{email2}'.")
-        return email1, email2
+        email1 = f"{first_name.lower()[0]}.{last_name.lower()}@{company_name.lower()}.com"
+        email2 = f"{first_name.lower()}.{last_name.lower()[0]}@{company_name.lower()}.com"
+        email3 = f"{first_name.lower()}_{last_name.lower()}@{company_name.lower()}.com"
+        email4 = f"{first_name.lower()}{last_name.lower()}@{company_name.lower()}.com"
+        logger.info(f"Generated email addresses: '{email1}', '{email2}', '{email3}', '{email4}'.")
+        return email1, email2, email3, email4
     elif first_name:
         email = f"{first_name.lower()}@{company_name.lower()}.com"
         logger.info(f"Generated email address: '{email}'.")
